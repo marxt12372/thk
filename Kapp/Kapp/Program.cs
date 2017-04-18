124f116de35d4832090c23d606ff4ae8e80241a4
Mart Ammon <mart@liovic.eu>
2017-04-18 09:46:23 +0300

    Lisatud veel 3 vanemat projekti

---
+++ Kapp/Kapp/Program.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kapp
+{
+	class Sahtel
+	{
+		private static List<Sahtel> _sahtlilist = new List<Sahtel>();
+
+		private Kapp _kapp;
+		private Vector3 _m66t;
+
+		public Sahtel(Kapp kapp, Vector3 m66t)
+		{
+			_kapp = kapp;
+			_m66t = m66t;
+
+			_sahtlilist.Add(this);
+		}
+
+		public Vector3 getM66t()
+		{
+			return _m66t;
+		}
+
+		public Kapp getKapp()
+		{
+			return _kapp;
+		}
+
+		public static List<Sahtel> getSahtliList()
+		{
+			return _sahtlilist;
+		}
+	}
+
+	class Kapp
+	{
+		private static List<Kapp> _kapilist = new List<Kapp>();
+
+		private string _asukoht;
+		private bool _lukustatav;
+		private bool _lukus;
+
+		public Kapp(string asukoht, bool lukustatav)
+		{
+			_asukoht = asukoht;
+			_lukustatav = lukustatav;
+
+			_kapilist.Add(this);
+		}
+
+		public List<Sahtel> getSahtlid()
+		{
+			List<Sahtel> sahtlilist = new List<Sahtel>();
+			foreach (Sahtel sahtel in Sahtel.getSahtliList())
+			{
+				if(sahtel.getKapp() == this)
+				{
+					sahtlilist.Add(sahtel);
+				}
+			}
+			return sahtlilist;
+		}
+
+		public Vector3 getMaht()
+		{
+			Vector3 maht = new Vector3();
+			Vector3 m66t;
+			foreach(Sahtel sahtel in this.getSahtlid())
+			{
+				m66t = sahtel.getM66t();
+				maht.X = maht.X + m66t.X;
+				maht.Y = maht.Y + m66t.Y;
+				maht.Z = maht.Z + m66t.Z;
+			}
+			return maht;
+		}
+
+		public double getKoguMaht()
+		{
+			Vector3 maht = getMaht();
+			return (maht.X * maht.Y * maht.Z);
+		}
+
+		public void lukusta()
+		{
+			if (_lukustatav)
+			{
+				if (_lukus == false)
+				{
+					_lukus = true;
+					Console.WriteLine("Kapp lukustati");
+				}
+			}
+			else
+			{
+				Console.WriteLine("see kappi ei ole lukustatav");
+			}
+		}
+
+		public void ava()
+		{
+			if (_lukustatav)
+			{
+				if (_lukus == true)
+				{
+					_lukus = false;
+					Console.WriteLine("Kapp avati");
+				}
+			}
+			else
+			{
+				Console.WriteLine("see kappi ei ole lukustatav");
+			}
+		}
+
+		public static List<Kapp> getKapiList()
+		{
+			return _kapilist;
+		}
+
+		public void showStats()
+		{
+			Console.WriteLine("-- " + _asukoht + " kapi andmed --");
+			Console.WriteLine("Lukustatav: " + _lukustatav + ", Lukus: " + _lukus);
+			Console.WriteLine("Sahtlite arv: " + getSahtlid().Count);
+			Console.WriteLine("Kogumaht: " + getKoguMaht());
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Kapp kapp1 = new Kapp("elutuba", true);
+			for (int i = 0; i < 7; i++)
+			{
+				Sahtel sahtel = new Sahtel(kapp1, new Vector3(5, 5, 7));
+			}
+			kapp1.lukusta();
+			kapp1.showStats();
+			kapp1.ava();
+			kapp1.showStats();
+
+			Kapp kapp2 = new Kapp("magamistuba", false);
+			for (int i = 0; i < 7; i++)
+			{
+				Sahtel sahtel = new Sahtel(kapp2, new Vector3(5, 5, 7));
+			}
+			kapp2.lukusta();
+			kapp2.showStats();
+			kapp2.ava();
+			kapp2.showStats();
+
+			Console.ReadKey();
+		}
+	}
+
+	class Vector3
+	{
+		public double X;
+		public double Y;
+		public double Z;
+
+		public Vector3(double _x, double _y, double _z)
+		{
+			X = _x;
+			Y = _y;
+			Z = _z;
+		}
+
+		public Vector3()
+		{
+
+		}
+	}
+}
---
+++ KlassiMuutuja/KlassiMuutuja/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlassiMuutuja
+{
+    class Punkt
+    {
+        public static List<Punkt> punktid = new List<Punkt>();
+        private static int loendur = 0;
+        private int _nr;
+        private int _x;
+        private int _y;
+        public Punkt(int x, int y)
+        {
+            _x = x;
+            _y = y;
+            _nr = ++loendur;
+            punktid.Add(this);
+        }
+
+        public void trykiKordinaadid()
+        {
+            Console.WriteLine("({0}) {1}:{2}", _nr, _x, _y);
+        }
+    }
+
+	class ValeIsikukood:Exception
+	{
+		public ValeIsikukood()
+		{
+			Console.WriteLine("Vale isikukood");
+		}
+	}
+
+	class Isikukood
+	{
+		private int _isikukood;
+
+		public Isikukood(int isikukood)
+		{
+			if(isikukood.ToString().Length == 11)
+			{
+				_isikukood = isikukood;
+			}
+			else throw new ValeIsikukood();
+		}
+	}
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			/*Punkt p1 = new Punkt(1, 1);
+            Punkt p2 = new Punkt(3, 2);
+            Punkt p3 = new Punkt(8, 6);
+			Punkt[] mas = new Punkt[10];
+			Punkt[] mas2 = new Punkt[10];
+			for (int i = 0; i < 10; i++)
+			{
+				mas[i] = new Punkt(i + 1, i + 1);
+			}
+			for (int i = 0; i < mas2.Length; i++)
+			{
+				mas2[i] = new Punkt(10 - i, i+1);
+			}
+			p1.trykiKordinaadid();
+            p2.trykiKordinaadid();
+            p3.trykiKordinaadid();
+			foreach (Punkt p in Punkt.punktid)
+			{
+                p.trykiKordinaadid();
+            }*/
+            Console.ReadKey();
+        }
+    }
+}
