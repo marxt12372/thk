8ff405a3523dc27fc278c55482908b615ef15699
Mart Ammon <mart@liovic.eu>
2017-04-21 15:17:07 +0300

    Meetodite asendus.

---
+++ MeetoditeAsendus/MeetoditeAsendus/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeetoditeAsendus
+{
+	class Inimene
+	{
+		protected int _vanus;
+
+		public Inimene(int vanus)
+		{
+			_vanus = vanus;
+		}
+
+		public virtual void YtleVanus()
+		{
+			Console.WriteLine("Olen " + _vanus + " aastane inimene.");
+		}
+	}
+
+	class Daam:Inimene
+	{
+		public Daam(int vanus):base(vanus)
+		{
+
+		}
+
+		public override void YtleVanus()
+		{
+			Console.WriteLine("Olen " + (_vanus-5) + " aastane daam.");
+		}
+	}
+	
+	class Beib:Daam
+	{
+		public Beib(int vanus):base(vanus)
+		{
+		}
+
+		public new virtual void YtleVanus()
+		{
+			Console.WriteLine("Beibi vanus ei ole sinu asi.");
+		}
+	}
+
+	sealed class KavalBeib : Beib
+	{
+		public KavalBeib(int vanus):base(vanus)
+		{
+		}
+
+		public override void YtleVanus()
+		{
+			Console.WriteLine("No mis sa arvad, kas olen " + (_vanus+2) + "?");
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			KavalBeib kb = new KavalBeib(17);
+			Console.Write("Kavalbeib: ");
+			kb.YtleVanus();
+			Beib b = new Beib(17), bkb = kb;
+			Console.Write("Beib: ");
+			b.YtleVanus();
+			Console.Write("Beib Kavalbeib: ");
+			bkb.YtleVanus();
+			Daam d = new Daam(40), db = b, dkb = kb;
+			Console.Write("Daam: ");
+			d.YtleVanus();
+			Console.Write("DaamBeib: ");
+			db.YtleVanus();
+			Console.Write("Daam Kavalbeib: ");
+			dkb.YtleVanus();
+			Inimene i = new Inimene(40), id = d, ib = b, ikb = kb;
+			Console.Write("Inimene: ");
+			i.YtleVanus();
+			Console.Write("Inimdaam: ");
+			id.YtleVanus();
+			Console.Write("Inimbeib: ");
+			ib.YtleVanus();
+			Console.Write("Inimene Kavalbeib: ");
+			ikb.YtleVanus();
+		}
+	}
+}
---
+++ MeetoditeAsendus2/MeetoditeAsendus2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeetoditeAsendus2
+{
+	class Punkt
+	{
+		protected int _x;
+		protected int _y;
+
+		public Punkt(int x, int y)
+		{
+			_x = x;
+			_y = y;
+		}
+
+		public virtual double kaugusNullist()
+		{
+			return Math.Sqrt(Math.Pow(Math.Abs(_x),2) + Math.Pow(Math.Abs(_y),2));
+		}
+
+		public void teataAndmed()
+		{
+			Console.WriteLine("--Punkt--");
+			Console.WriteLine("X: " + _x);
+			Console.WriteLine("Y: " + _y);
+		}
+	}
+
+	class RuumiPunkt:Punkt
+	{
+		private int _z;
+
+		public RuumiPunkt(int x, int y, int z):base(x, y)
+		{
+		}
+
+		public override double kaugusNullist()
+		{
+			return Math.Sqrt(Math.Pow(Math.Abs(_x), 2) + Math.Pow(Math.Abs(_y), 2) + Math.Pow(Math.Abs(_z), 2));
+		}
+
+		public new void teataAndmed()
+		{
+			Console.WriteLine("--Punkt--");
+			Console.WriteLine("X: " + _x);
+			Console.WriteLine("Y: " + _y);
+			Console.WriteLine("Z: " + _z);
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Punkt p1 = new Punkt(1, 1);
+			Punkt p2 = new Punkt(1, 1), p3 = p1;
+			p1.teataAndmed();
+			p2.teataAndmed();
+			p3.teataAndmed();
+
+			RuumiPunkt rp1 = new RuumiPunkt(1, 1, 1);
+			RuumiPunkt rp2 = new RuumiPunkt(1, 1, 1), rp3 = rp1;
+			rp3.teataAndmed();
+			Console.WriteLine("rp3 on: " + rp3.GetType().ToString());
+		}
+	}
+}
