fefccd0c82a0c6b41cbc86ce14b10a80ad09713a
Mart Ammon <mart@liovic.eu>
2017-05-08 15:08:55 +0300

    Uuendused

--- ArvestusÜL/ArvestusÜL/Program.cs
+++ ArvestusÜL/ArvestusÜL/Program.cs
@@ -8,6 +8,8 @@ namespace ArvestusÜL
 {
 	class Auto
 	{
+		protected static int _autodearv = 0;
+
 		protected string _mark;
 		protected int _kohti;
 		protected int _reisijaid;
@@ -17,13 +19,16 @@ namespace ArvestusÜL
 			_mark = mark;
 			_kohti = kohti;
 			_reisijaid = 0;
+
+			_autodearv++;
 		}
 
-		public void ytleMark()
+		public virtual void ytleMark()
 		{
 			Console.WriteLine("Mark: " + _mark);
 			Console.WriteLine("Kohti: " + _kohti);
 			Console.WriteLine("Reisijaid: " + _reisijaid);
+			Console.WriteLine("Sõidukeid maamunal: " + _autodearv);
 		}
 
 		public int VabuKohti
@@ -37,8 +42,45 @@ namespace ArvestusÜL
 				}
 			}
 		}
+
+		public void SiseneAutosse(int arv)
+		{
+			VabuKohti -= arv;
+		}
 	}
 
+	class Kaubik:Auto
+	{
+
+		protected double _kaubaruum;
+		protected double _kaupa;
+
+		public Kaubik(string mark, double kaubaruum):base(mark, 0)
+		{
+			_kaubaruum = kaubaruum;
+			_kaupa = 0;
+		}
+
+		public override void ytleMark()
+		{
+			Console.WriteLine("Mark: " + _mark);
+			Console.WriteLine("Kaubaruum: " + _kaubaruum);
+			Console.WriteLine("Kaupa: " + _kaupa);
+			Console.WriteLine("Sõidukeid maamunal: " + _autodearv);
+		}
+
+		public double VabaKaubaruum
+		{
+			get { return _kaubaruum - _kaupa; }
+			set
+			{
+				if (value >= 0 && value < _kaubaruum)
+				{
+					_kaupa = _kaubaruum - value;
+				}
+			}
+		}
+	}
 
 	class Program
 	{
@@ -48,6 +90,11 @@ namespace ArvestusÜL
 			audi.ytleMark();
 			audi.VabuKohti = 3;
 			audi.ytleMark();
+
+			Kaubik ford = new Kaubik("Ford", 4.5);
+			ford.ytleMark();
+			ford.VabaKaubaruum = 4;
+			ford.ytleMark();
 		}
 	}
 }
