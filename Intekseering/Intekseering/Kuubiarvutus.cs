89b32d6eebb17d6f4204ecca354462552bcbd237
Opilane TTHK <opilane@tthk.ee>
2018-01-11 09:39:08 +0200

    Lisatud kuubiarvutus

---
+++ Intekseering/Intekseering/Kuubiarvutus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intekseering
+{
+	class Kuubiarvutis
+	{
+		public Kuubiarvutis()
+		{
+		}
+
+		public int this[int nr] {
+			get { return nr * nr * nr; }
+		}
+	}
+}
